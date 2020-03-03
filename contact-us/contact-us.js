45e3cd70464255871644fefbfad1f86ba77c7c6d
Margaret Wairimu Kimani-1040 <kimanimargaret46@gmail.com>
2020-03-03 17:00:50 +0300

    added comment section

--- contact-us/contact-us.js
+++ contact-us/contact-us.js
@@ -29,3 +29,16 @@ setTimeout(function(){
         $('.menu footer button').click();
     }, (count * 100) + 500 );
 }, 1000);
+
+
+function myMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("comment").value;
+    if (email !== "" && name !== "" && message !== "") {
+        // document.getElementById("pop").innerHTML =
+        alert("Hello" + " " + name + " we have received your message. Thank you for reaching out to us.");
+    } else if (email == "" || name !== "" || message !== "") {
+        alert("Please fill in all the details");
+    }
+}
