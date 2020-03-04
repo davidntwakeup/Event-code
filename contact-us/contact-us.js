ce62633c336ec04ab149c2f7601ee7b407d91cd8
Margaret Wairimu Kimani-1040 <kimanimargaret46@gmail.com>
2020-03-04 13:04:04 +0300

    indent code

--- contact-us/contact-us.js
+++ contact-us/contact-us.js
@@ -1,33 +1,33 @@
 var $els = $('.menu a, .menu header');
 var count = $els.length;
-var grouplength = Math.ceil(count/3);
+var grouplength = Math.ceil(count / 3);
 var groupNumber = 0;
 var i = 1;
-$('.menu').css('--count',count+'');
-$els.each(function(j){
-    if ( i > grouplength ) {
+$('.menu').css('--count', count + '');
+$els.each(function (j) {
+    if (i > grouplength) {
         groupNumber++;
-        i=1;
+        i = 1;
     }
-    $(this).attr('data-group',groupNumber);
+    $(this).attr('data-group', groupNumber);
     i++;
 });
-$('.menu footer button').on('click',function(e){
+$('.menu footer button').on('click', function (e) {
     e.preventDefault();
-    $els.each(function(j){
-        $(this).css('--top',$(this)[0].getBoundingClientRect().top + ($(this).attr('data-group') * -15) - 20);
-        $(this).css('--delay-in',j*.1+'s');
-        $(this).css('--delay-out',(count-j)*.1+'s');
+    $els.each(function (j) {
+        $(this).css('--top', $(this)[0].getBoundingClientRect().top + ($(this).attr('data-group') * -15) - 20);
+        $(this).css('--delay-in', j * .1 + 's');
+        $(this).css('--delay-out', (count - j) * .1 + 's');
     });
     $('.menu').toggleClass('closed');
     e.stopPropagation();
 });
 // run animation once at beginning for demo
-setTimeout(function(){
+setTimeout(function () {
     $('.menu footer button').click();
-    setTimeout(function(){
+    setTimeout(function () {
         $('.menu footer button').click();
-    }, (count * 100) + 500 );
+    }, (count * 100) + 500);
 }, 1000);
 
 
